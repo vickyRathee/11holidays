96e62e4c493016f7c79b8aca6ea89c2d835f4323
Vikash Rathee <vickyrathee@users.noreply.github.com>
2026-07-03 17:17:28 +0530

    use cache

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -3,10 +3,19 @@ import { COUNTRIES_WITH_SLUG } from '@/lib/countries-data';
 import { fetchOccasions } from '../lib/occassion-api';
 import { currentYear } from '../lib/holidays-api';
 import { getCloudflareContext } from '@opennextjs/cloudflare';
+import { cacheLife } from 'next/cache';
 
-export const revalidate = 3600; // 1 hour
+export const dynamic = 'force-dynamic';
 
 const BASE_URL = 'https://11holidays.com';
+
+async function getCachedOccasions(env: CloudflareEnv) {
+  'use cache';
+
+  cacheLife('hours');
+
+  return fetchOccasions(env);
+}
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const holidaysEntries = COUNTRIES_WITH_SLUG.flatMap((x) => [
@@ -39,7 +48,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const { env } = await getCloudflareContext({ async: true });
 
-  const occasions = await fetchOccasions(env);
+  const occasions = await getCachedOccasions(env);
 
   const occasionsEntries = occasions.map((x) => ({
     url: `${BASE_URL}/holidays/${x.url}`,
